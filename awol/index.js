e0b2061f9338fbe7ac909fa607528e99b392e4b5
STITCH <mfstitch@gmail.com>
2018-08-14 15:20:01 -0400

    ready?

--- awol/index.js
+++ awol/index.js
@@ -1,47 +1,47 @@
-const base = require('./base.less');
-const buttons = require('./buttons.less');
-const colors = require('./colors.less');
-const editor = require('./editor.less');
-const findAndReplace = require('./find-and-replace.less');
-const git = require('./git.less');
-const index = require('./index.less');
-const keyBinding = require('./key-binding.less');
-const License = require('./LICENSE');
-const lists = require('./lists.less');
-const package = require('./package.json');
-const panels = require('./panels.less');
-const progress = require('./progress.less');
-const sites = require('./sites.less');
-const statusBar = require('./status-bar.less');
-const styles = require('./styles.less');
-const syntaxVariables = require('./syntax-variables.less');
-const tabs = require('./tabs.less');
-const text = require('./text.less');
-const tooltips = require('./tooltips.less');
-const treeView = require('./tree-view.less');
-const uiVariables = require('./ui-variables.less');
+import Base from './BASE.less';
+import buttons from './buttons.less';
+import Colores from './COLORES.less';
+import editor from './editor.less';
+import findAndReplace from './find-and-replace.less';
+import git from './git.less';
+import Index from './INDEX.js';
+import index from './index.less';
+import KeYedEntries from './KEYed-ENTRIES.less';
+import License from './LICENSE';
+import Lists from './LISTS.less';
+import Package from './Package.json';
+import panels from './panels.less';
+import progress from './progress.less';
+import Sitess from './SITESS.less';
+import StatusBars from './STATUS-BARS.less';
+import Styleez from './STYLEEZ.less';
+import syntaxVariables from './syntax-VARIABLES.less';
+import Texts from './TEXTS.less';
+import Tooltips from './TOOLTIPS.less';
+import Treez from './TREEZ.less';
+import UtiVariables from './UTI-variables.less';
 
-module.exports = {
- base,
- buttons,
- colors,
- editor,
- findAndReplace,
- git,
- index,
- keyBinding,
- License,
- lists,
- package,
- panels,
- progress,
- sites,
- statusBar,
- styles,
- syntaxVariables,
- tabs,
- text,
- tooltips,
- treeView,
- uiVariables
+export {
+  Base,
+  buttons,
+  Colores,
+  editor,
+  findAndReplace,
+  git,
+  Index,
+  index,
+  KeYedEntries,
+  License,
+  Lists,
+  Package,
+  panels,
+  progress,
+  Sitess,
+  StatusBars,
+  Styleez,
+  syntaxVariables,
+  Texts,
+  Tooltips,
+  Treez,
+  UtiVariables
 };
